251f59a68df6327b7460635619cca1440bd2132c
Aleem Khaliq <aleemkhaliq@ymail.com>
2017-02-16 23:57:52 +0000

    Tunneling
    Began to implement the tunnels, not yet working

---
+++ Assets/Scripts/TunnelHome.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TunnelHome : MonoBehaviour {
+
+    private Player player;
+    private TunnelMessage tunnelMsg;
+    private Vector2 tunnel;
+        
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        tunnelMsg = GameObject.FindGameObjectWithTag("T_Message").GetComponent<TunnelMessage>();
+        tunnel = gameObject.transform.position;
+    }
+	
+    // Checks to see if the player has entered over the area of the tunnel
+	void OnTriggerEnter2D(Collider2D area)
+    {
+        if (area.CompareTag("Player"))
+        {
+            Debug.Log("Area crossed");
+            // Sets Display boolean to true so that the tunnel message prompt is displayed
+            tunnelMsg.Display = true;
+
+            if(Input.GetKeyDown(KeyCode.T))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+        else
+        {
+            tunnelMsg.Display = false;
+        }
+    }
+     
+}
