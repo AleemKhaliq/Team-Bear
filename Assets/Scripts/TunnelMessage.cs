c5356d7eecd79f720198c2e92ccf10b67319b8cb
Rexter51 <edex@hotmail.co.uk>
2017-02-20 07:16:40 +0000

    Outdoor level stuff
    added a outdoor level(basic) and camera lock

---
+++ Assets/Scripts/TunnelMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelMessage : MonoBehaviour {
+
+    public SpriteRenderer render;
+
+    public bool Display { get; set; }
+
+    // Use this for initialization
+    void Start ()
+    {
+        render = GetComponent<SpriteRenderer>();
+        render.enabled = false;
+        Display = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //Displays the sprite when the Display is set to true
+        render.enabled = Display;
+	}
+}
