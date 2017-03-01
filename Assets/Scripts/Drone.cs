80dda3698c5c806564cefdda3e1438761412c79c
Aleem Khaliq <aleemkhaliq@ymail.com>
2017-03-01 23:51:22 +0000

    Small drone improvements
    Small addition to the scene and script

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drone : MonoBehaviour {
+public class Drone : MonoBehaviour
+{
+    private int health;
+    public int maximumHealth;
 
+    public bool faceRight;
+    public bool faceLeft;
 
+    private Player player;
+    
 
 	// Use this for initialization
 	void Start ()
