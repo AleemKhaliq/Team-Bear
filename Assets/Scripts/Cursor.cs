3f6c35ee87ec0192e72a9d4f3bdb28fe6edc2a64
Tewy96 <Tewys-other-account1@hotmail.co.uk>
2017-03-23 07:13:51 +0000

    Map Screen (Not Done)

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public LevelMarker marker;
+
+	// Use this for initialization
+	void Start ()
+    {
+        marker = GameObject.FindGameObjectWithTag("Marker").GetComponent<LevelMarker>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0 && marker.levelNo != 11)
+        {
+            transform.position = marker.nextLevel.transform.position + new Vector3(0, 0.5f);
+            marker = marker.nextLevel;
+            Debug.Log("Right");
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0 && marker.levelNo != 1)
+        {
+            transform.position = marker.previousLevel.transform.position + new Vector3(0, 0.5f);
+            marker = marker.previousLevel;
+            Debug.Log("Left");
+        }
+        transform.position = marker.transform.position;
+    }
+}
---
+++ Assets/Scripts/LevelMarker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMarker : MonoBehaviour
+{
+    public int levelNo;
+    public bool isOpen;
+    public bool isDone;
+    public LevelMarker nextLevel;
+    public LevelMarker previousLevel;
+
+    public Animator anim;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        isOpen = false;
+        isDone = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        anim.SetBool("IsOpen", isOpen);
+        anim.SetBool("IsDone", isDone);
+    }
+}
