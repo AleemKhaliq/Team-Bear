7b80f3a0ce1928fbaae2c872bfdc72b82ada853e
Tewy96 <Tewys-other-account1@hotmail.co.uk>
2017-04-05 21:26:36 +0100

    Basic Boss

---
+++ Assets/Scripts/BossHand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHand : MonoBehaviour
+{
+    public bool hitObject;
+    public bool active;
+
+    private Rigidbody2D getBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        //getBody = gameObject.GetComponent<Rigidbody2D>();
+        active = false;
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+        //StartCoroutine(Slam());
+
+    }
+
+    IEnumerator Slam()
+    {
+        active = true;
+        if(!hitObject)
+        {
+            transform.position = new Vector2 (transform.position.x, transform.position.y - 1);
+        }
+        yield return new WaitForSeconds(0.25f);
+        active = false;
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+}
---
+++ Assets/Scripts/BossHandTrigger.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHandTrigger : MonoBehaviour {
+
+    public BossHand bossHand;
+
+    private Player player;
+    private Vector2 spike;
+
+    // Use this for initialization
+    void Start()
+    {
+        bossHand = gameObject.GetComponentInParent<BossHand>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        spike = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.isTrigger != true)
+        {
+            bossHand.hitObject = true;
+
+        }
+        if (col.CompareTag("Player"))
+        {
+            player.Damage(1);
+            Debug.Log("Hit");
+            int direction;
+            if (player.transform.position.x > spike.x)
+            {
+                direction = 1;
+            }
+            else
+            {
+                direction = -1;
+            }
+            StartCoroutine(player.Knockback(0.02f, direction, 5f));
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.isTrigger != true)
+        {
+            bossHand.hitObject = true;
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.isTrigger != true)
+        {
+            bossHand.hitObject = false;
+
+        }
+    }
+}
