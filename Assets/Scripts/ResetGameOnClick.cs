c1b7ecb43740792cc3a978fd2448b50e17d59f73
Rexter51 <edex@hotmail.co.uk>
2017-04-06 07:21:39 +0100

    Main Menu Done

---
+++ Assets/Scripts/ResetGameOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGameOnClick : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            PlayerPrefs.DeleteAll();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
